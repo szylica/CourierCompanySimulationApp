ce2cd0c9e11aebefad029dd2b64f9d297afb4092
szylica <szymonsta12@onet.pl>
2025-09-02 22:59:56 +0200

    test(ParcelMachineRepository): add test for ParcelMachineRepository using docker testcontainers

---
+++ src/test/java/org/szylica/database/repository/impl/ParcelMachineRepositoryImplTest.java
@@ -0,0 +1,7 @@
+package org.szylica.database.repository.impl;
+
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers(disabledWithoutDocker = true)
+public class ParcelMacineRepositoryImplTest {
+}
